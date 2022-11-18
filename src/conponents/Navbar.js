ff8b869024264499168a63d6233b0cf61beec053
thedeveloperj <holderjamese@gmail.com>
2022-11-18 12:30:55 +0000

    11/18/2022

--- src/conponents/Navbar.js
+++ src/conponents/Navbar.js
@@ -63,7 +63,7 @@ export default function Navbar() {
                 </div>
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                   {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-                  <a href="#" class="
+                  <a href="projects" class="
                       inline-flex
                       items-center
                       border-b-2 border-indigo-500
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     element: <App/>,
   },
   {
-    path: "/",
+    path: "/projects",
     element: <Projects/>,
   },
 ]);
